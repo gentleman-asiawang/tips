8f4fc83091f7c4f10dfd24eac8ca6c99330a83da
yzwang <gentelmanwang@gmail.com>
2025-09-08 16:09:26 +0800

    change some thing

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -51,8 +51,8 @@
         </p>
         <p class="p2">
           Wu W., Cui C., Zhu Y., Chen J., Wang Y., Liu Z., Gao H., Ou G.-Z., Liu C., Tao M., Chen Y., Pan R., Zhang G.,
-          Cai H., Yang J., Chen X., Zhou X., Wang S., & Shen X.-X. 2025. Exploring protein functions and remote homologs
-          across the insect tree of life with structural genomics.
+          Cai H., Yang J., Chen X., Zhou X., Wang S., & Shen X.-X. 2025. Structural genomics sheds light on protein
+          functions and remote homologs across the insect tree of life.
         </p>
 
 
