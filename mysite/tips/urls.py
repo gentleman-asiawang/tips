8c6e2f8a3f7dd75e583ebc489ece288b0c12f39b
yzwang <gentelmanwang@gmail.com>
2025-11-14 11:30:48 +0800

    change some code

--- mysite/tips/urls.py
+++ mysite/tips/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from tips.views.querybyid import query_by_id
 from tips.views.views import login, get_orders, ReceiveFileAPIView, delete_all_temp_files, GetServerLoadAPIView, get_file_info
-from tips.views.file2ui import DownloadData, DownloadTable, GetPDBFile
+from tips.views.file2ui import DownloadData, DownloadTable, get_pdb_file
 from tips.views.querybypdb import QueryByPDB
 from tips.views.querybysequence import query_by_sequence
 from tips.views.prunetree import prune_tree
@@ -10,7 +10,7 @@ urlpatterns = [
     path('query_by_pdb/', QueryByPDB.as_view(), name='query_by_pdb'),
     path('query_by_sequence/', query_by_sequence, name='query_by_sequence'),
     path('query_by_id/', query_by_id, name='query_by_id'),
-    path('get_pdb_file/', GetPDBFile.as_view(), name='get_pdb_file'),
+    path('get_pdb_file/', get_pdb_file, name='get_pdb_file'),
     path('prune_tree/', prune_tree, name='prune_tree'),
     path('download_data/', DownloadData.as_view(), name='download_data'),
     path('download_table/', DownloadTable.as_view(), name='download_table'),
--- mysite/tips/views/file2ui.py
+++ mysite/tips/views/file2ui.py
@@ -1,5 +1,6 @@
 import os
 import re
+import io
 import zipfile
 import tempfile
 import subprocess
@@ -11,8 +12,8 @@ from tips.views.my_module import UuidManager
 from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl.styles import Font, Alignment, Border, Side
 from django.http import FileResponse, HttpResponse
-from rest_framework.exceptions import ParseError
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from tips.models import DataInfo
@@ -21,52 +22,51 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class GetPDBFile(APIView):
+@api_view(['GET'])
+def get_pdb_file(request):
     """
-       根据 tips_id 返回 PDB 文件，ATOM 列小于 1 的值放大 100 倍。
-       GET 参数: tipsid
+    根据 tips_id 返回 PDB 文件，ATOM 列小于 1 的值放大 100 倍。
+    GET 参数: tipsid
     """
-    def get(self, request):
-        tips_id = request.query_params.get('tipsid')
-        if not tips_id:
-            return Response({'error': 'tipsid parameter is required'}, status=status.HTTP_400_BAD_REQUEST)
+    tips_id = request.query_params.get('tipsid')
+    if not tips_id:
+        return Response({'error': 'tipsid parameter is required'}, status=status.HTTP_400_BAD_REQUEST)
 
-        pdb_file_name = DataInfo.objects.filter(tips_id=tips_id).values_list('basename', flat=True).first()
-        if not pdb_file_name:
-            return Response({'error': 'Tips ID not found.'}, status=status.HTTP_404_NOT_FOUND)
+    pdb_file_name = DataInfo.objects.filter(tips_id=tips_id).values_list('basename', flat=True).first()
+    if not pdb_file_name:
+        return Response({'error': 'Tips ID not found.'}, status=status.HTTP_404_NOT_FOUND)
 
-        match = re.match(r'^(.*?)_at_', pdb_file_name)
-        if match:
-            filename = match.group(1)
-        else:
-            return Response({'error': 'Invalid basename format'}, status=status.HTTP_400_BAD_REQUEST)
-        pdb_file_path = os.path.join('/data/Data/Structure', filename, pdb_file_name + ".pdb")  # 替换为实际路径
-        if not os.path.exists(pdb_file_path):
-            return Response({'error': 'File not found'}, status=status.HTTP_404_NOT_FOUND)
+    match = re.match(r'^(.*?)_at_', pdb_file_name)
+    if match:
+        filename = match.group(1)
+    else:
+        return Response({'error': 'Invalid basename format'}, status=status.HTTP_400_BAD_REQUEST)
+    pdb_file_path = os.path.join('/data/Data/Structure', filename, pdb_file_name + ".pdb")  # 替换为实际路径
+    if not os.path.exists(pdb_file_path):
+        return Response({'error': 'File not found'}, status=status.HTTP_404_NOT_FOUND)
 
-        modified_lines = []
+    modified_lines = []
 
-        with open(pdb_file_path,'r') as file:
-            for line in file:
-                if line.startswith("ATOM"):
-                    columns = list(line)
-                    try:
-                        original_value = float(line[61:66].strip())
-                    except ValueError:
-                        modified_lines.append(line)
-                        continue
-                    if original_value < 1:
-                        modified_value = original_value * 100
-                        formatted_value = f"{modified_value:6.2f}"
-                        columns[61:66] = formatted_value[:8]
-                        modified_line = "".join(columns)
-                        modified_lines.append(modified_line)
-                    else:
-                        modified_lines.append(line)
+    with open(pdb_file_path,'r') as file:
+        for line in file:
+            if line.startswith("ATOM"):
+                columns = list(line)
+                try:
+                    original_value = float(line[61:66].strip())
+                except ValueError:
+                    modified_lines.append(line)
+                    continue
+                if original_value < 1:
+                    modified_value = original_value * 100
+                    formatted_value = f"{modified_value:6.2f}"
+                    columns[61:66] = formatted_value[:8]
+                    modified_line = "".join(columns)
+                    modified_lines.append(modified_line)
                 else:
                     modified_lines.append(line)
-
-        return HttpResponse(modified_lines, content_type='chemical/x-pdb')
+            else:
+                modified_lines.append(line)
+    return HttpResponse(modified_lines, content_type='chemical/x-pdb')
 
 
 class DownloadTable(APIView):
@@ -149,51 +149,66 @@ class DownloadData(APIView):
         if uuid not in UuidManager.uuid_storage:
             return Response({'error': 'uuid does not exist'}, status=status.HTTP_404_NOT_FOUND)
 
-
-        data = request.data
-        if not isinstance(data, dict):
-            # 如果不是字典，则说明 JSON 无效
-            raise ParseError('Invalid JSON body')
-        tips_id = data.get('tips_id')
-        down_seq = data.get('sequence', False)
-        if not tips_id or not isinstance(tips_id, list):
-            return Response({'error': 'tips_id must be a non-empty list'}, status=status.HTTP_400_BAD_REQUEST)
-
-
+        tips_id = request.data.get('tips_id')
         if not tips_id:
             return Response({'error': 'tips_id parameter is required'}, status=status.HTTP_400_BAD_REQUEST)
+        if not tips_id or not isinstance(tips_id, list):
+            return Response({'error': 'tips_id must be a non-empty list'}, status=status.HTTP_404_NOT_FOUND)
+
+        # 三种情况，pdb、sequence、both
+        down_type = request.data.get('down_type', 'pdb') # 默认只下载pdb
+        if not down_type:
+            return Response({'error': 'down_type parameter is required'}, status=status.HTTP_400_BAD_REQUEST)
+        if down_type not in ['pdb', 'sequence', 'both']:
+            return Response({'error': 'Download type error'}, status=status.HTTP_404_NOT_FOUND)
 
         data_infos = DataInfo.objects.filter(tips_id__in=tips_id)
-        if not data_infos.exists():
+        found_ids = set(data_infos.values_list('tips_id', flat=True))
+        missing_ids = set(tips_id) - found_ids
+        if missing_ids:
+            return Response({'warning': 'Some tips_id not found', 'missing_ids': list(missing_ids)}, status=status.HTTP_404_NOT_FOUND)
+        if found_ids is None:
             return Response({'error': 'No data found for given tips_id'}, status=status.HTTP_404_NOT_FOUND)
 
-        full_file_paths = [self.get_full_path(di.basename) for di in data_infos]
-
-        seq_path = None
-        if down_seq:
+        fasta_text = None
+        if down_type == 'both':
             try:
-                seq_path = self.get_sequence(tips_id, uuid)
+                fasta_text = self.get_sequence(tips_id)
             except subprocess.CalledProcessError as e:
                 return Response({'error': 'Blast command failed', 'details': e.stderr},
                                 status=status.HTTP_400_BAD_REQUEST)
-
-        if len(full_file_paths) == 1 and not seq_path:
-            outpath = full_file_paths[0]
-        else:
-            outpath = self.create_zip(full_file_paths, seq_path, uuid)
-        return FileResponse(open(outpath, 'rb'), as_attachment=True, filename='files.zip')
+        elif down_type == 'pdb': # 只输出结构的情况，如果只输出一个就不需要打包，否则需要打包返回
+            full_file_paths = [self.get_full_path(di.basename) for di in data_infos]
+            if len(full_file_paths) == 1: # 只输出一个，直接返回序列文件
+                full_path = full_file_paths[0]
+                ext = os.path.splitext(full_path)[1]
+                filename = f"select{ext}"
+                return FileResponse(open(full_file_paths[0], 'rb'), as_attachment=True, filename=filename)
+            else: # 选择了多个文件，要打包后返回
+                buffer = self.create_zip_in_memory(full_file_paths, fasta_text)
+                return FileResponse(buffer, as_attachment=True, filename='select.zip')
+        else: # 只输出序列的情况，不需要压缩，直接返回
+            try:
+                fasta_text = self.get_sequence(tips_id)
+            except subprocess.CalledProcessError as e:
+                return Response({'error': 'Blast command failed', 'details': e.stderr},
+                                status=status.HTTP_400_BAD_REQUEST)
+            if fasta_text is None:
+                return Response({'error': 'Fasta file not found'}, status=status.HTTP_404_NOT_FOUND)
+            fasta_bytes = BytesIO(fasta_text.encode("utf-8"))
+            return FileResponse(fasta_bytes, as_attachment=True, filename='select.fasta')
 
 
     @staticmethod
-    def create_zip(pdb_file_list, seq_path, uuid):
+    def create_zip(pdb_file_list, fasta_text, uuid):
         temp_dir = f'{settings.TEMP_DIR}/{uuid}'
         os.makedirs(temp_dir, exist_ok=True)
         with tempfile.NamedTemporaryFile(delete=False, dir=f'{settings.TEMP_DIR}/{uuid}', suffix='.zip') as temp_zip_file:
             zip_file_path = temp_zip_file.name
             UuidManager.add_entry(uuid, 'zip', zip_file_path)
             with zipfile.ZipFile(zip_file_path, 'w') as zipf:
-                if seq_path:
-                    zipf.write(seq_path, arcname=f'data/select.fasta')
+                if fasta_text is not None:
+                    zipf.write(fasta_text, arcname=f'data/select.fasta')
                 if len(pdb_file_list) == 1:
                     zipf.write(pdb_file_list[0], arcname=f'data/{os.path.basename(pdb_file_list[0])}')
                 else:
@@ -201,6 +216,29 @@ class DownloadData(APIView):
                         logger.debug(filename)
                         zipf.write(filename, arcname=f'data/Structure/{os.path.basename(filename)}')
             return zip_file_path
+
+    @staticmethod
+    def create_zip_in_memory(pdb_file_list, fasta_text):
+        buffer = io.BytesIO()  # 内存中存放 ZIP
+
+        with zipfile.ZipFile(buffer, 'w', zipfile.ZIP_DEFLATED) as zipf:
+            # 写入 FASTA（直接内存文本）
+            if fasta_text:
+                zipf.writestr("select.fasta", fasta_text)
+
+            # 写入 pdb 文件（读取本地文件内容到内存 ZIP）
+            if len(pdb_file_list) == 1:
+                with open(pdb_file_list[0], 'rb') as f:
+                    zipf.writestr(f"{os.path.basename(pdb_file_list[0])}", f.read())
+            else:
+                for filename in pdb_file_list:
+                    with open(filename, 'rb') as f:
+                        zipf.writestr(f"data/Structure/{os.path.basename(filename)}", f.read())
+
+        buffer.seek(0)  # 回到文件开头
+
+        # 返回 FileResponse 给前端
+        return FileResponse(buffer, as_attachment=True, filename='select.zip')
 
     @staticmethod
     def get_full_path(basename):
@@ -211,14 +249,13 @@ class DownloadData(APIView):
 
 
     @staticmethod
-    def get_sequence(seq_list, uuid):
-        temp_dir = f'{settings.TEMP_DIR}/{uuid}'
-        os.makedirs(temp_dir, exist_ok=True)
-        with tempfile.NamedTemporaryFile(delete=False, dir=f'{settings.TEMP_DIR}/{uuid}', suffix='.fasta') as temp_seq_file:
-            temp_seq_file_path = temp_seq_file.name
-            UuidManager.add_entry(uuid, 'fasta', temp_seq_file_path)
-            list_input = ','.join(seq_list)
-            blast_cmd = ['blastdbcmd', '-db', '/tips_db/blast_db/all_db/all', '-entry', list_input, '-dbtype', 'prot',
-                         '-out', temp_seq_file_path]
-        subprocess.run(blast_cmd, capture_output=True, text=True, check=True)
-        return temp_seq_file_path
+    def get_sequence(seq_list):
+        seq_ids = ','.join(seq_list)
+        blast_cmd = [
+            'blastdbcmd',
+            '-db', '/tips_db/blast_db/all_db/all',
+            '-entry', seq_ids,
+            '-dbtype', 'prot'
+        ]
+        result = subprocess.run(blast_cmd, capture_output=True, text=True, check=True)
+        return result.stdout
