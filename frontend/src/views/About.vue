0646e5cb4bd404c19606e525ebad317c50ff4c8c
Weiyin Wu <59970244+TigerWindWood@users.noreply.github.com>
2026-01-13 17:11:29 +0800

    Update About.vue

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -20,7 +20,7 @@
       <p class="primary-citation">
         Wu W., Cui C., Zhu Y., Chen J., Zhuang Q., Wang Y., Liu Z., Gao H., Ou G.-Z., Liu C., Tao M., Chen Y., Pan R.,
         Zhang G.,
-        Cai H., Yang J., Chen X., Zhou X., Wang S., & Shen X.-X. 2025. Structural genomics sheds light on
+        Cai H., Yang J., Chen X., Zhou X., Wang S., & Shen X.-X. 2026. Structural genomics sheds light on
         protein functions and remote homologs across the insect tree of life. <em>Cell Research</em>, in press.
       </p>
       <p class="info-text">Please also cite the relevant paper if you use the corresponding option:</p>
