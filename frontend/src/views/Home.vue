4c7db23ae9adc1d421e350980579ba9276c93438
Weiyin Wu <59970244+TigerWindWood@users.noreply.github.com>
2026-01-13 16:25:38 +0800

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -50,7 +50,7 @@
         <p class="p2">
           Wu W., Cui C., Zhu Y., Chen J., Zhuang Q., Wang Y., Liu Z., Gao H., Ou G.-Z., Liu C., Tao M., Chen Y., Pan R., Zhang G.,
           Cai H., Yang J., Chen X., Zhou X., Wang S., & Shen X.-X. 2025. Structural genomics sheds light on protein
-          functions and remote homologs across the insect tree of life.
+          functions and remote homologs across the insect tree of life. <em>Cell Research</em>, in press.
         </p>
       </div>
       <div class="text-container2">
